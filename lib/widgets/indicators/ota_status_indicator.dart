eff2365e2c8a50435aed47d62bbc92db928128c7
Teal Bauer <teal@starsong.eu>
2025-10-07 14:59:16 +0200

    feat(ota): add rebooting status handling to indicator

--- lib/widgets/indicators/ota_status_indicator.dart
+++ lib/widgets/indicators/ota_status_indicator.dart
@@ -8,6 +8,7 @@ import 'indicator_light.dart';
 class _Icons {
   static const String downloading = 'librescoot-ota-status-downloading.svg';
   static const String installing = 'librescoot-ota-status-installing.svg';
+  static const String rebooting = 'librescoot-ota-status-waiting-for-reboot.svg';
 }
 
 class OtaStatusIndicator extends StatelessWidget {
@@ -21,7 +22,7 @@ class OtaStatusIndicator extends StatelessWidget {
     
     // Check if DBC update is ongoing
     final dbcStatus = otaData.dbcStatus;
-    final isOtaOngoing = dbcStatus == 'downloading' || dbcStatus == 'installing';
+    final isOtaOngoing = dbcStatus == 'downloading' || dbcStatus == 'installing' || dbcStatus == 'rebooting';
     
     // Only show when scooter is unlocked and DBC update is ongoing
     final isUnlocked = vehicleState.state == ScooterState.readyToDrive || 
@@ -33,13 +34,33 @@ class OtaStatusIndicator extends StatelessWidget {
 
     final color = isDark ? Colors.white : Colors.black;
     final updateVersion = otaData.dbcUpdateVersion;
-    final actionText = dbcStatus == 'downloading' ? 'Downloading' : 'Installing';
+
+    final String actionText;
+    final String iconAsset;
+    switch (dbcStatus) {
+      case 'downloading':
+        actionText = 'Downloading';
+        iconAsset = _Icons.downloading;
+        break;
+      case 'installing':
+        actionText = 'Installing';
+        iconAsset = _Icons.installing;
+        break;
+      case 'rebooting':
+        actionText = 'Waiting for reboot';
+        iconAsset = _Icons.rebooting;
+        break;
+      default:
+        actionText = 'Updating';
+        iconAsset = _Icons.downloading;
+    }
+
     final versionText = updateVersion.isNotEmpty ? ' Librescoot $updateVersion' : ' update';
 
     return Tooltip(
       message: '$actionText$versionText',
       child: IndicatorLight(
-        icon: IndicatorLight.svgAsset(dbcStatus == 'downloading' ? _Icons.downloading : _Icons.installing),
+        icon: IndicatorLight.svgAsset(iconAsset),
         isActive: true,
         size: 24.0,
         activeColor: color,
